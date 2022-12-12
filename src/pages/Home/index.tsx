dd11b6776030d32393155a1b93a52b2f3b1af2a5
KENNEDY COSTA <kennedy.costa.valenet@gmail.com>
2022-12-12 13:02:56 -0300

    Home styling

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,24 @@
+import { ReactComponent as MainImage } from 'assets/images/main-image.svg';
+
+import './styles.css'
+
 const Home = () => {
-    return(
-        <div className="theme-color-default">
-             <h1>{"Hello World from MovieFlix =]"}</h1>
+  return (
+
+    <div className="container theme-color-default">
+      <div className="home-container">
+        <div className="text-container">
+          <h5>Avalie Filmes</h5>
+          <p>Diga o que você achou de seu filme favorito</p>
+          <MainImage />
         </div>
-    );
-}
+        <div className="login-container">
+            <h5>LOGIN CARD</h5>
+        </div>
+      </div>
+    </div>
+
+  );
+};
 
 export default Home;
