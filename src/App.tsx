1a387fe13a2aa2fb0d14b7af18bd88adda604ba6
KENNEDY COSTA <kennedy.costa.valenet@gmail.com>
2022-12-13 12:21:59 -0300

    Movie Catalog styling

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import Navbar from 'components/Navbar';
 import './assets/styles/custom.scss';
 import './App.css';
 import Home from 'pages/Home';
+import Catalog from 'pages/Private/MovieCatalog';
+import MovieCatalog from 'pages/Private/MovieCatalog';
 
 function App() {
   return (
     <>
       <Navbar />
-      <Home />
+      <MovieCatalog />
     </>
   );
 }
---
+++ src/pages/Private/MovieCatalog/index.tsx
@@ -0,0 +1,15 @@
+import './styles.css';
+
+const MovieCatalog = () => {
+  return (
+    <div className="theme-color-default">
+      <h5>Tela de listagem de filmes</h5>
+      <div className="list-container">
+        <p>{'Acessar /movies/1'}</p>
+        <p>{'Acessar /movies/1'}</p>
+      </div>
+    </div>
+  );
+};
+
+export default MovieCatalog;
